74d7736e700d881b0380dc86e2d88e6517b02cc2
Tenzin Lekmay <tenzinlekmay139@gmail.com>
2025-12-28 18:29:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9057907979-5a743",
+  "appId": "1:905903765204:web:b26bac828442d5c44a2d41",
+  "apiKey": "AIzaSyDl_juCJr9geiuSr2O31E5-TcpWGCaRroI",
+  "authDomain": "studio-9057907979-5a743.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "905903765204"
+};
